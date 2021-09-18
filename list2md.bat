6f0f7923e9fbae0fe58dd74d41762c88ca7a259e
KentaGoto <gotokentag@gmail.com>
2021-09-18 09:33:02 +0900

    1st

---
+++ list2md.bat
@@ -0,0 +1,26 @@
+@echo off
+
+rem OVERVIEW:
+rem Folder creation batch file from list (*.txt).
+rem
+rem USAGE:
+rem Drag and drop a text file (*.txt) with the name of the folder 
+rem you want to create into this batch file.
+rem
+rem NOTE:
+rem The format of the list (*.txt) is Shift-JIS.
+
+
+echo Folder:
+echo.
+
+for /f "delims=" %%a in (%1) do (
+	md "%%a"
+	echo %%a
+)
+
+echo.
+echo Done!
+echo Please Enter to exit.
+
+pause > nul
